4d1ab5ea407ded4aadb62601f28fa07eceedcce0
Ali Hamza <ali.mhamza06@gmail.com>
2026-04-09 15:48:48 +0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 use std::io::{self, Write};
 use std::net::TcpListener;
 
-fn main() -> Result<(), io::Error> {
+fn main() -> io::Result<()> {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379")?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n")?;
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+    loop {
+        if let Ok((mut stream, _)) = listener.accept() {
+            stream.write_all(b"+PONG\r\n")?;
+        } else {
+            break;
         }
     }
 
