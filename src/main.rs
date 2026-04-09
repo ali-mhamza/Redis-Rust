ecce3af2d7aa75bfc5c85ffed78d1391b979b55b
Ali Hamza <ali.mhamza06@gmail.com>
2026-04-09 16:16:44 +0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 use std::io;
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn respond_to_connection(mut stream: TcpStream) -> io::Result<()> {
+    loop {
+        stream.read(&mut [0; 128])?;
+        stream.write_all(b"+PONG\r\n")?;
+    }
+}
 
 fn main() -> io::Result<()> {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379")?;
+    let mut handles = Vec::new();
 
-    match listener.accept() {
-        Ok((mut stream, _)) => {
-            loop {
-                stream.read(&mut [0; 128])?;
-                stream.write_all(b"+PONG\r\n")?;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handles.push(thread::spawn(move || -> io::Result<()> {
+                    respond_to_connection(stream)?;
+
+                    Ok(())
+                }));
+            },
+            Err(e) => {
+                eprintln!("Error: {e}");
             }
-        },
-        Err(e) => {
-            eprintln!("Error: {e}");
         }
+    }
+    
+    for handle in handles {
+        handle.join().unwrap();
     }
 
     Ok(())
