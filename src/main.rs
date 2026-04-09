ef1ebb9942a54abc6e0d89c2cdd316dabc2e1493
Ali Hamza <ali.mhamza06@gmail.com>
2026-04-09 15:39:57 +0400

    Remove unnecessary parts.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-#![allow(unused_imports)]
-
 use std::io::{self, Write};
 use std::net::TcpListener;
 
 fn main() -> Result<(), io::Error> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379")?;
