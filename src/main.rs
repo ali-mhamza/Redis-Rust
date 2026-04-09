b80a0580618717072ae827f0b8053d636b5d252c
Ali Hamza <ali.mhamza06@gmail.com>
2026-04-09 22:08:34 +0400

    Additional debug step.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use std::io;
 use std::net::{TcpListener};
 
 fn main() -> io::Result<()> {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379")?;
     connect::handle_connections(&listener)?;
 
