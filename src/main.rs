4eff0408786cc2883995a5a5fa9c3238aa735767
Ali Hamza <ali.mhamza06@gmail.com>
2026-04-13 18:00:58 +0400

    Handle custom port argument.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,23 @@ pub mod resp;
 pub mod utils;
 
 pub use commands::{DataTable, Stream, StreamID};
+use std::env;
 use std::io;
 use std::net::{TcpListener};
 
+fn parse_port() -> i64 {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 3 {
+        args[2].parse::<i64>().unwrap()
+    } else {
+        6379
+    }
+}
+
 fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379")?;
+    let addr = format!("127.0.0.1:{}", parse_port());
+    let listener = TcpListener::bind(addr)?;
     connect::handle_connections(&listener)?;
 
     Ok(())
